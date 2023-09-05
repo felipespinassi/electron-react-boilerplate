6d6a00dccabeb21d83a8dc47ba0f39693fc64acb
Felipe <felipe_spinassi@hotmail.com>
2023-09-05 00:28:03 -0300

    feat: add boilerplate electron + react + typescript

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+
+export default function App(): JSX.Element {
+  return (
+   <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'100vh'}} >
+    
+    <p>hello electron</p>
+
+   </div>
+  )
+}
