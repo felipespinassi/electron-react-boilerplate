8fc0108bcd9b68647ae6aa57e2e9ea3dee2e6f37
Felipe <felipe_spinassi@hotmail.com>
2023-09-05 00:58:43 -0300

    feat: add antd

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,27 @@
+import { Row, Button } from 'antd'
+import { useState } from 'react'
 
 export default function App(): JSX.Element {
-  return (
-   <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'100vh'}} >
-    
-    <p>hello electron</p>
+  const [counter, setCounter] = useState(0)
 
-   </div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        flexDirection: 'column'
+      }}
+    >
+      <Row>
+        <Button onClick={() => setCounter(counter + 1)} type="primary">
+          Electron + react + antd
+        </Button>
+      </Row>
+      <Row style={{ paddingTop: 20 }}>
+        <p>{counter}</p>
+      </Row>
+    </div>
   )
 }
